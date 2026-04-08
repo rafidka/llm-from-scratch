5dcd58313a7824a75bdaff12c08a822eecc3fccc
Rafid K. <rafidka@gmail.com>
2026-04-08 22:49:14 +0000

    Add a training Bash script

---
+++ train.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+uv run python -m llm_from_scratch.examples.training.train_cloud \
+    --model_size medium \
+    --epochs 1 \
+    --batch_size 32 \
+    --checkpoint_dir /storage/checkpoints 
