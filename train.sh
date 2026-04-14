a65ce2cb24ff00721b32631d9a677e4052219fde
Rafid K. <rafidka@gmail.com>
2026-04-13 18:02:41 -0700

    Avoid absolute paths in train.sh

--- train.sh
+++ train.sh
@@ -3,4 +3,4 @@ uv run python -m llm_from_scratch.examples.training.train_cloud \
     --model_size medium \
     --epochs 1 \
     --batch_size 32 \
-    --checkpoint_dir /storage/checkpoints 
+    --checkpoint_dir checkpoints 
